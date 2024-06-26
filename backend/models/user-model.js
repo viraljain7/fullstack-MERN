f9a74867386913a37ae439245411398336323f8d
viraljain7 <veerljain1234@gmail.com>
2024-06-26 17:00:56 +0530

    Add userModel

---
+++ backend/models/user-model.js
@@ -0,0 +1,17 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

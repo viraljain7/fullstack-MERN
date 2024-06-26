51f8bb8c998fd425f71de11fad6b332edf57fb3c
viraljain7 <veerljain1234@gmail.com>
2024-06-26 13:02:09 +0530

    Add auth router

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import router from "./Router/auth-router.js";
+
+const app = express();
+
+app.use("/api/auth", router)
+
+
+
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log('RUNNING ON PORT :: ', PORT);
+})

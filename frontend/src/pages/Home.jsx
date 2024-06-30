2e41d8b0040306a77b4413870239543de7bb09a6
viraljain7 <veerljain1234@gmail.com>
2024-06-30 21:35:30 +0530

    Add Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,24 @@
+import './common.css';
 
-const Home = () => {
+const HomePage = () => {
     return (
-        <div>
-            Home
+        <div className="home-container">
+            <header className="home-header">
+                <h1>Welcome to Our Application</h1>
+            </header>
+            <section className="home-content">
+                <p>This is the home page of our application. Here you can find some basic information and navigate to other sections.</p>
+                <div className="features">
+                    <h2>Features</h2>
+                    <ul>
+                        <li>Feature 1: Description of feature 1.</li>
+                        <li>Feature 2: Description of feature 2.</li>
+                        <li>Feature 3: Description of feature 3.</li>
+                    </ul>
+                </div>
+            </section>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default HomePage;

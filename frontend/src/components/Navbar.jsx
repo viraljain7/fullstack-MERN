afde0e9c442ff351e63eed2c01cf4d8760da2c6d
viraljain7 <veerljain1234@gmail.com>
2024-06-30 21:35:17 +0530

    Add Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,23 @@
 // src/components/Navbar.js
 
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // Importing CSS for Navbar component
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <ul className="navbar-menu">
-                <li className="navbar-item">
-                    <Link to="/" className="navbar-link">Home</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/about" className="navbar-link">About</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/contact" className="navbar-link">Contact</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/service" className="navbar-link">Service</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/login" className="navbar-link">Login</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/register" className="navbar-link">Register</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className='left'>Company</div>
+
+      <div className="right">
+        <NavLink to="/" className="navbar-link">Home</NavLink>
+        <NavLink to="/about" className="navbar-link">About</NavLink>
+        <NavLink to="/contact" className="navbar-link">Contact</NavLink>
+        <NavLink to="/service" className="navbar-link">Service</NavLink>
+        <NavLink to="/login" className="navbar-link">Login</NavLink>
+        <NavLink to="/register" className="navbar-link">Register</NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
